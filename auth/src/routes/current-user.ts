322408cc62cdec88f0748e9bff07d81cbbbf28e3
Ngoc Tong <ngoctong421@gmail.com>
2024-11-11 14:32:15 +0700

    creating route handlers

---
+++ auth/src/routes/current-user.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/api/users/currentuser', async (req, res) => {
+  res.send('Hi there!');
+});
+
+export { router as currentUserRouter };
