d14912a1cc8b272b55c89f7e1afce6cebf098732
Ngoc Tong <ngoctong421@gmail.com>
2024-12-17 01:07:52 +0700

    index to app refactor

---
+++ auth/src/app.js
@@ -0,0 +1,46 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = void 0;
+const express_1 = __importDefault(require("express"));
+const express_session_1 = __importDefault(require("express-session"));
+const current_user_1 = require("./routes/current-user");
+const signin_1 = require("./routes/signin");
+const signout_1 = require("./routes/signout");
+const signup_1 = require("./routes/signup");
+const error_handler_1 = require("./middlewares/error-handler");
+const not_found_error_1 = require("./errors/not-found-error");
+const app = (0, express_1.default)();
+exports.app = app;
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.set('trust proxy', 1);
+app.use((0, express_session_1.default)({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 3600000,
+    },
+}));
+app.use(current_user_1.currentUserRouter);
+app.use(signin_1.signinRouter);
+app.use(signout_1.signoutRouter);
+app.use(signup_1.signupRouter);
+app.all('*', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    next(new not_found_error_1.NotFoundError());
+}));
+app.use(error_handler_1.errorHandler);
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,44 +1,6 @@
-import express from 'express';
-import session from 'express-session';
 import mongoose from 'mongoose';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.set('trust proxy', 1);
-app.use(
-  session({
-    secret: 'your-secret-key',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      maxAge: 3600000,
-    },
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async (req, res, next) => {
-  next(new NotFoundError());
-});
-
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
