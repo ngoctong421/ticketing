a08473fd1c41b31518cfae2827cb2ca456a9d1e7
Ngoc Tong <ngoctong421@gmail.com>
2024-12-16 17:47:14 +0700

    returning the current user

--- auth/src/routes/current-user.js
+++ auth/src/routes/current-user.js
@@ -14,8 +14,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.currentUserRouter = void 0;
 const express_1 = __importDefault(require("express"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const router = express_1.default.Router();
 exports.currentUserRouter = router;
 router.get('/api/users/currentuser', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    res.send('Hi there!');
+    var _a;
+    if (!((_a = req.session) === null || _a === void 0 ? void 0 : _a.jwt)) {
+        res.send({ currentUser: null });
+    }
+    try {
+        const payload = jsonwebtoken_1.default.verify(req.session.jwt, process.env.JWT_KEY);
+        res.send({ currentUser: payload });
+    }
+    catch (err) {
+        res.send({ currentUser: null });
+    }
 }));
