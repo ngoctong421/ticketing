45e70af34958746fc19699080613056a6a254a18
Ngoc Tong <ngoctong421@gmail.com>
2024-12-16 15:30:10 +0700

    accessing env variables in a pod

--- auth/src/routes/signup.js
+++ auth/src/routes/signup.js
@@ -49,7 +49,7 @@ router.post('/api/users/signup', validateRequestBody(userSchema), (req, res, nex
     const user = user_1.User.build({ email, password });
     yield user.save();
     // Generate JWT
-    const userJwt = jsonwebtoken_1.default.sign({ id: user.id, email: user.email }, 'secret');
+    const userJwt = jsonwebtoken_1.default.sign({ id: user.id, email: user.email }, process.env.JWT_KEY);
     // Store it on session object
     req.session.jwt = userJwt;
     res.status(201).send(user);
