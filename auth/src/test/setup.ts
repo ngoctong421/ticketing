7d2de651bdcd55a56aae1dab8d73a34b91f06bc5
Ngoc Tong <ngoctong421@gmail.com>
2024-12-17 12:27:27 +0700

    test environment setup

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,27 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+
+import { app } from '../app';
+
+let mongod: any;
+
+beforeAll(async () => {
+  mongod = await MongoMemoryServer.create();
+
+  const uri = mongod.getUri();
+
+  await mongoose.connect(uri);
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db!.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongod.stop();
+  await mongoose.connection.close();
+});
