fe110bab068249d86bdab4788479190671cee2d1
Ngoc Tong <ngoctong421@gmail.com>
2024-12-17 15:26:13 +0700

    easy auth solution

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,16 +2,24 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('responds with detail about current user', async () => {
-  await request(app)
+  const authResponse = await request(app)
     .post('/api/users/signup')
     .send({
       email: 'test@test.io',
       password: '1234',
     })
     .expect(201);
+  const cookie = authResponse.get('Set-Cookie');
+
+  if (!cookie) {
+    throw new Error('Cookie not set after signup');
+  }
 
   const response = await request(app)
     .get('/api/users/currentuser')
+    .set('Cookie', cookie)
     .send()
     .expect(200);
+
+  expect(response.body.email).toEqual('test@test.io');
 });
