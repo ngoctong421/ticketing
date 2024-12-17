69550c55f9c878153fcc690e63ef9a6361643b0d
Ngoc Tong <ngoctong421@gmail.com>
2024-12-17 14:19:01 +0700

    testing invalid input

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,34 @@ it('returns a 201 on successful signup', async () => {
     })
     .expect(201);
 });
+
+it('returns a 400 with an invalid email', async () => {
+  return await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test',
+      password: '1234',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+  return await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.io',
+      password: '1',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.io' })
+    .expect(400);
+  await request(app)
+    .post('/api/users/signup')
+    .send({ password: '1234' })
+    .expect(400);
+});
