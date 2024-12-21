40a73c0859cbc9a43caad2af02de793f09c5d521
Ngoc Tong <ngoctong421@gmail.com>
2024-12-21 16:43:46 +0700

    fetching data using ssr

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,32 @@
-const LandingPage = () => {
-  return <h1>Landing Page</h1>;
+import axios from 'axios';
+
+import useRequest from '../hooks/use-request';
+
+const LandingPage = ({ id, email }) => {
+  return email ? <h1>Hello {email}</h1> : <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = () => {
-  return {};
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    const { data } = await axios
+      .get(
+        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+        {
+          headers: req.headers,
+        }
+      )
+      .catch((err) => {
+        console.log(err.message);
+      });
+
+    return data;
+  } else {
+    const { data } = await axios.get('/api/users/currentuser').catch((err) => {
+      console.log(err.message);
+    });
+
+    return data;
+  }
 };
 
 export default LandingPage;
