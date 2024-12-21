34bdeede5897b96fd2067ae8cb7de714030c7f9d
Ngoc Tong <ngoctong421@gmail.com>
2024-12-21 16:36:15 +0700

    change currentuser route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,17 +1,11 @@
 import express from 'express';
 
 import { currentUser } from '../middlewares/current-user';
-import { requireAuth } from '../middlewares/require-auth';
 
 const router = express.Router();
 
-router.get(
-  '/api/users/currentuser',
-  currentUser,
-  requireAuth,
-  async (req, res) => {
-    res.send(req.currentUser);
-  }
-);
+router.get('/api/users/currentuser', currentUser, async (req, res) => {
+  res.send(req.currentUser ?? {});
+});
 
 export { router as currentUserRouter };
