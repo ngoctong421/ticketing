bfc3ecf261dcebcd16f66876455484ab1a691a55
Ngoc Tong <ngoctong421@gmail.com>
2024-12-22 23:45:45 +0700

    content on the landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,7 @@
-import axios from 'axios';
-
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ id, email }) => {
-  return email ? <h1>Hello {email}</h1> : <h1>Landing Page</h1>;
+  return email ? <h1>You are signed in</h1> : <h1>You are NOT sign in</h1>;
 };
 
 LandingPage.getInitialProps = async (context) => {
