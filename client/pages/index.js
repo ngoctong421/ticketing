9ead2369d5969617548276345755677183a8e0cb
Ngoc Tong <ngoctong421@gmail.com>
2024-12-22 23:39:08 +0700

    a reusable api client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,32 +1,15 @@
 import axios from 'axios';
 
-import useRequest from '../hooks/use-request';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ id, email }) => {
   return email ? <h1>Hello {email}</h1> : <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    const { data } = await axios
-      .get(
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-        {
-          headers: req.headers,
-        }
-      )
-      .catch((err) => {
-        console.log(err.message);
-      });
+LandingPage.getInitialProps = async (context) => {
+  const { data } = await buildClient(context).get('/api/users/currentuser');
 
-    return data;
-  } else {
-    const { data } = await axios.get('/api/users/currentuser').catch((err) => {
-      console.log(err.message);
-    });
-
-    return data;
-  }
+  return data;
 };
 
 export default LandingPage;
