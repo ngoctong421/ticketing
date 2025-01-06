333372ceb52adb2ad540a13763d60a21182148fe
Ngoc Tong <ngoctong421@gmail.com>
2025-01-06 23:16:29 +0700

    change express-session to cookie-session

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,14 +3,9 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signout', async (req, res) => {
-  req.session.destroy((err) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Unable to log out');
-    } else {
-      res.send({});
-    }
-  });
+  req.session = null;
+
+  res.send({});
 });
 
 export { router as signoutRouter };
