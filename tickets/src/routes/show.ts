ce3f79ecd90d84de3e88c92575d9bee4d7644550
Ngoc Tong <ngoctong421@gmail.com>
2025-01-13 16:36:24 +0700

    create show routes

--- tickets/src/routes/show.ts
+++ tickets/src/routes/show.ts
@@ -1,18 +1,21 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { NotFoundError } from 'tickets-common';
 
 import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.get('/api/tickets/:id', async (req: Request, res: Response) => {
-  const ticket = await Ticket.findById(req.params.id);
+router.get(
+  '/api/tickets/:id',
+  async (req: Request, res: Response, next: NextFunction) => {
+    const ticket = await Ticket.findById(req.params.id);
 
-  if (!ticket) {
-    throw new NotFoundError();
+    if (!ticket) {
+      return next(new NotFoundError());
+    }
+
+    res.send(ticket);
   }
-
-  res.send(ticket);
-});
+);
 
 export { router as showTicketRouter };
