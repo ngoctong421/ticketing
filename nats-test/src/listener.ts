5744c6ed7556dffa6ca3a77816a99fc67f8d9f66
Ngoc Tong <ngoctong421@gmail.com>
2025-01-17 22:40:08 +0700

    queue groups

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -8,7 +8,10 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
 
-  const subcription = stan.subscribe('ticket:created');
+  const subcription = stan.subscribe(
+    'ticket:created',
+    'orders-service-queue-group'
+  );
 
   subcription.on('message', (msg: Message) => {
     const data = msg.getData();
