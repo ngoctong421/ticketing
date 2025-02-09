da2c96e0c811f2130f5d7b5cd9ab708c5df55365
Ngoc Tong <ngoctong421@gmail.com>
2025-02-09 20:42:04 +0700

    singleton implementation

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -12,6 +13,7 @@ const start = async () => {
   }
 
   try {
+    await natsWrapper.connect('ticketing', 'tickets', 'http://nats-srv:4222');
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to database');
   } catch (err) {
