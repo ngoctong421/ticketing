8dbdc80d25f2238429784fa30f88079fb88e985e
Ngoc Tong <ngoctong421@gmail.com>
2025-02-18 06:57:05 +0700

    skaffolding a few routes handlers

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -2,10 +2,10 @@ import cookieSession from 'cookie-session';
 import express from 'express';
 import { errorHandler, NotFoundError, currentUser } from 'tickets-common';
 
-import { createTicketRouter } from './routes/new';
-import { showTicketRouter } from './routes/show';
-import { indexTicketRouter } from './routes';
-import { updateTicketRouter } from './routes/update';
+import { createOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
+import { indexOrderRouter } from './routes';
+import { deleteOrderRouter } from './routes/delete';
 
 const app = express();
 
@@ -22,10 +22,10 @@ app.use(
 
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(createOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all('*', async (req, res, next) => {
   next(new NotFoundError());
