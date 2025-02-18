9ef33402e66ca31813a8849e48eccebd54a9ae57
Ngoc Tong <ngoctong421@gmail.com>
2025-02-18 07:11:28 +0700

    subtle service coupling

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,24 @@
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import { requireAuth, validateRequest } from 'tickets-common';
+import { z } from 'zod';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
+const orderSchema = z.object({
+  ticketId: z
+    .string()
+    .nonempty({ message: 'TicketId must be provided' })
+    .refine((val) => mongoose.Types.ObjectId.isValid(val)),
 });
+
+router.post(
+  '/api/orders',
+  requireAuth,
+  validateRequest(orderSchema),
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as createOrderRouter };
