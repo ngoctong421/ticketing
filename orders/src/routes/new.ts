eaa034de6fcec7d80cc98419a2ae663ca7599d9b
Ngoc Tong <ngoctong421@gmail.com>
2025-02-19 11:05:20 +0700

    order creation logic

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,9 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import mongoose from 'mongoose';
-import { requireAuth, validateRequest } from 'tickets-common';
+import { NotFoundError, requireAuth, validateRequest } from 'tickets-common';
 import { z } from 'zod';
+
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
@@ -16,7 +18,23 @@ router.post(
   '/api/orders',
   requireAuth,
   validateRequest(orderSchema),
-  async (req: Request, res: Response) => {
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { ticketId } = req.body;
+
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      return next(new NotFoundError());
+    }
+
+    // Make sure that this ticket is not already reserved
+
+    // Calculate an expiration date for this order
+
+    // Build the order and save it to the database
+
+    // Publish an event saying that an order was created
+
     res.send({});
   }
 );
