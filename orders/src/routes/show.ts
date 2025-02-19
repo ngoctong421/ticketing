f78932071d646bf3deead571e5aeb58cb4820e0d
Ngoc Tong <ngoctong421@gmail.com>
2025-02-19 16:07:58 +0700

    fetching individual orders

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,26 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import { NotAuthorizedError, NotFoundError, requireAuth } from 'tickets-common';
+
+import { Order } from '../models/order';
 
 const router = express.Router();
 
-router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response, next: NextFunction) => {
+    const order = await Order.findById(req.params.orderId).populate('ticket');
+
+    if (!order) {
+      return next(new NotFoundError());
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      return next(new NotAuthorizedError());
+    }
+
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
