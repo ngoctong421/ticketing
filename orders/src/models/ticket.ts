464dc9ab0596d22a1e6df225368d99392c00f1db
Ngoc Tong <ngoctong421@gmail.com>
2025-02-20 23:26:01 +0700

    id adjustment

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -4,6 +4,7 @@ import { OrderStatus } from 'tickets-common';
 import { Order } from './order';
 
 interface TicketAttrs {
+  id: string;
   title: string;
   price: number;
 }
@@ -41,7 +42,11 @@ const ticketSchema = new mongoose.Schema(
 );
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
-  return new Ticket(attrs);
+  return new Ticket({
+    _id: attrs.id,
+    title: attrs.title,
+    price: attrs.price,
+  });
 };
 ticketSchema.methods.isReserved = async function () {
   const existingOrder = await Order.findOne({
