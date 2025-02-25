f1346be691c83e67d022f1d62475024df238840f
Ngoc Tong <ngoctong421@gmail.com>
2025-02-25 16:42:18 +0700

    testing occ

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -1,1 +1,33 @@
-it('implements optimistic concurrency control', async () => {});
+import mongoose from 'mongoose';
+
+import { Ticket } from '../ticket';
+
+it('implements optimistic concurrency control', async () => {
+  // Create an instance of a ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 5,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+  });
+
+  // Save the ticket to the database
+  await ticket.save();
+
+  // Fetch the ticket twice
+  const firstInstance = await Ticket.findById(ticket.id);
+  const secondInstance = await Ticket.findById(ticket.id);
+
+  // Make two separate changes to the tickets we fetch
+  firstInstance!.set({ price: 10 });
+  secondInstance!.set({ price: 20 });
+
+  // Save the first fetched ticket
+  await firstInstance!.save();
+
+  // Save the second fetched ticket and expect an error
+  try {
+    await secondInstance!.save();
+  } catch (err) {
+    return;
+  }
+});
