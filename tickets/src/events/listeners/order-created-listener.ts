23fbf5bbe3ab5f65682cb0c317f0ad0dab44962e
Ngoc Tong <ngoctong421@gmail.com>
2025-02-27 16:57:05 +0700

    reserving a ticket

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -2,10 +2,28 @@ import { Listener, OrderCreatedEvent, Subjects } from 'tickets-common';
 import { Message } from 'node-nats-streaming';
 
 import { queueGroupName } from './queue-group-name';
+import { Ticket } from '../../models/ticket';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    // find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    // if no ticket, throw error
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    // mark the ticket as being reserved by setting its orderId property
+    ticket.set({ orderId: data.id });
+
+    // save the ticket
+    await ticket.save();
+
+    // ack the message
+    msg.ack();
+  }
 }
