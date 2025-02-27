6c7668b184beae99c807b0f139184ca4e816e553
Ngoc Tong <ngoctong421@gmail.com>
2025-02-27 17:09:14 +0700

    setup for testing reservation

---
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+import { OrderCreatedEvent, OrderStatus } from 'tickets-common';
+import { Message } from 'node-nats-streaming';
+
+import { OrderCreatedListener } from '../order-created-listener';
+import { natsWrapper } from '../../../nats-wrapper';
+import { Ticket } from '../../../models/ticket';
+
+const setup = async () => {
+  // create an instance of the listener
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  // create and save a ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 10,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+  });
+  await ticket.save();
+
+  // create the fake data event
+  const data: OrderCreatedEvent['data'] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    status: OrderStatus.Created,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+    expiresAt: new Date().toISOString(),
+    ticket: {
+      id: ticket.id,
+      price: ticket.price,
+    },
+  };
+
+  // create the message object
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, ticket, data, msg };
+};
