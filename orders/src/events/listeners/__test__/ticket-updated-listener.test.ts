509e53ee51fb8c3b1d104a9e6727e32d0ea03872
Ngoc Tong <ngoctong421@gmail.com>
2025-02-27 14:56:54 +0700

    out-of-order events

--- orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
+++ orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -60,6 +60,7 @@ it('does not call ack if the event has a skipped version number', async () => {
   const { listener, ticket, data, msg } = await setup();
 
   data.version = 10;
+
   try {
     await listener.onMessage(data, msg);
   } catch (err) {}
