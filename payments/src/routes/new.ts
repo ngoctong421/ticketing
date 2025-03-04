b19a60da12b48fcc3431e8dfd58e025e30516f5f
Ngoc Tong <ngoctong421@gmail.com>
2025-03-04 12:55:21 +0700

    validating order payment

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,6 +1,15 @@
-import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from 'tickets-common';
+import express, { NextFunction, Request, Response } from 'express';
+import {
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from 'tickets-common';
 import { z } from 'zod';
+
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -13,7 +22,21 @@ router.post(
   '/api/payments',
   requireAuth,
   validateRequest(paymentSchema),
-  async (req: Request, res: Response) => {
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      next(new NotFoundError());
+    }
+    if (order!.userId !== req.currentUser!.id) {
+      next(new NotAuthorizedError());
+    }
+    if (order!.status === OrderStatus.Cancelled) {
+      next(new BadRequestError('Cannot pay for an cancelled order'));
+    }
+
     res.send({ success: true });
   }
 );
