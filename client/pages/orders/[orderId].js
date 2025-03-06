ff5647b0fa4d7f546accb36a58ee1d0056b67c53
Ngoc Tong <ngoctong421@gmail.com>
2025-03-06 18:40:07 +0700

    programmatic navigation to wildcard routes

---
+++ client/pages/orders/[orderId].js
@@ -0,0 +1,16 @@
+import useRequest from '../../hooks/use-request';
+
+const OrderShow = () => {
+  // const { doRequest, errors } = useRequest({
+  //   url: '/api/orders',
+  //   method: 'post',
+  //   body: {
+  //     ticketId: ticket.id,
+  //   },
+  //   onSuccess: (order) => console.log(order),
+  // });
+
+  return <div>OrderShow</div>;
+};
+
+export default OrderShow;
