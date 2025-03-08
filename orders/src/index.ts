b845ec19b4a371f7af057398212aa327dd53ccfb
Ngoc Tong <ngoctong421@gmail.com>
2025-03-08 23:48:10 +0700

    update services

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -13,6 +13,7 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
+
   if (!process.env.NATS_URL) {
     throw new Error('NATS_URL must be defined');
   }
@@ -22,6 +23,7 @@ const start = async () => {
   if (!process.env.NATS_CLIENT_ID) {
     throw new Error('NATS_CLIENT_ID must be defined');
   }
+
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
   }
