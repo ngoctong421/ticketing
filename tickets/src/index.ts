8a764aee96b9cad27d31e66102522d112f73d9b8
Ngoc Tong <ngoctong421@gmail.com>
2025-03-08 19:33:31 +0700

    add startup message to services

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,22 +6,20 @@ import { OrderCreatedListener } from './events/listeners/order-created-listener'
 import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start = async () => {
+  console.log('Starting up...');
+
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
-
   if (!process.env.NATS_URL) {
     throw new Error('NATS_URL must be defined');
   }
-
   if (!process.env.NATS_CLUSTER_ID) {
     throw new Error('NATS_CLUSTER_ID must be defined');
   }
-
   if (!process.env.NATS_CLIENT_ID) {
     throw new Error('NATS_CLIENT_ID must be defined');
   }
-
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
   }
